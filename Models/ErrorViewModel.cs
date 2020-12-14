a070332884f28b65a019c8c91ccc89521758aafe
Enrique Cerda <jenrique.cerda@gmail.com>
2020-12-14 13:15:30 -0600

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stations.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
